1120e67acdc8c6e0f5908feb03b024f345c31f4d
Chris Inch <github@chrisinch.com>
2015-10-08 14:21:43 -0400

    Removed the repeated (inverse) mapping between characters and morse. Not needed.

--- morsefy.js
+++ morsefy.js
@@ -38,46 +38,6 @@ var morse = {
     " " :  "/"
 };
 
-var chars = {
-    ".-"  : "a",
-    "-...": "b",
-    "-.-.": "c",
-    "-.." : "d",
-    "."   : "e",
-    "..-.": "f",
-    "--." : "g",
-    "....": "h",
-    ".."  : "i",
-    ".---": "j",
-    "-.-" : "k",
-    ".-..": "l",
-    "--"  : "m",
-    "-."  : "n",
-    "---" : "o",
-    ".--.": "p",
-    "--.-": "q",
-    ".-." : "r",
-    "..." : "s",
-    "-"   : "t",
-    "..-" : "u",
-    "...-": "v",
-    ".--" : "w",
-    "-..-": "x",
-    "-.--": "y",
-    "--..": "z",
-    ".----": "1",
-    "..---": "2",
-    "...--": "3",
-    "....-": "4",
-    ".....": "5",
-    "-....": "6",
-    "--...": "7",
-    "---..": "8",
-    "----.": "9",
-    "-----": "0",
-    "/"    : " "
-};
-
 function Morsefy(){
     return this;
 }
@@ -104,8 +64,11 @@ Morsefy.prototype = {
         for (var i=0; i<words.length; i++) {
             var character = words[i].split(" ");
             for (var j=0; j<character.length; j++) {
-                if (chars[character[j]])
-                    decoding += chars[character[j]];
+                for (var code in morse) {
+                    if (morse[code] == character[j]) {
+                        decoding += code;
+                    }
+                }
             }
             decoding += " ";
         }
